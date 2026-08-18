538f3623be72acc163da9278befc8ac03b7faf7f
Ozioko Anthony <oziokoanthony@gmail.com>
2026-08-18 23:17:59 +0100

    python day 2

---
+++ class work/class-work-day2.py
@@ -0,0 +1,24 @@
+
+# shopping_cart = [{"car", "food", "apples", 12, 14, 29, 23.0}, {"parle-g", "fibre-active"}, set(), set()]
+
+
+# for elements in shopping_cart:
+#   print(type(elements))
+  
+
+
+
+#   for i in range(1,7):
+   
+#     if i % 2 != 0:
+#        print(f"{i} is odd")
+
+# [i for i in range(1, 7) if i % 2 != 0]
+
+# import this
+
+
+
+user_input = input("tekk me something")
+while user_input != "quit":
+user_input = input("tell me something")
